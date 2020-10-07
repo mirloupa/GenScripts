dc73239219d1ce023fca12d00ce76f63171c62f4
Louisse Paola Mirabueno <43784584+mirloupa@users.noreply.github.com>
2020-10-07 18:56:03 +0100

    Update extract_effectors.sh
    Find effectors in PREFFECTOR output and save in new file with .res extension.

--- SH/extract_effectors.sh
+++ SH/extract_effectors.sh
@@ -1,5 +1,5 @@
 # Find effectors in PREFFECTOR output and save in new file with .res extension.
-
+ 
 for file in /data2/scratch2/mirabl/Xf_proj/Sequences/WGS/DNA_fasta/*.fasta; do
   file_short=$(basename $file | sed s/".fasta"//g)
   echo $file_short
